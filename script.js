21579af065fe73bfee4199fcfc6b7c71199460d6
BidyutBikashBoruah9365 <bidyutbikashboruah849@gmail.com>
2024-05-18 21:43:29 +0530

    expenseTracker

--- script.js
+++ script.js
@@ -7,10 +7,13 @@ expenseForm.addEventListener("submit", function(e){
     e.preventDefault()
     const descriptionValue = description.value
     const amountValue = amount.value
-    if (descriptionValue == " "){
-        return this.addEventListener
+    if (descriptionValue.trim() === "" || isNaN(amountValue) || amountValue <= 0){
+    alert("Please enter a valid description or amount")
+    return
     }
     addExpense(descriptionValue, amountValue)
+    description.value = ""
+    amount.value = ""
 })
 
 function addExpense(descriptionValue,amountValue){
@@ -18,7 +21,16 @@ function addExpense(descriptionValue,amountValue){
     console.log(amountValue)
     const li = document.createElement("li")
     li.innerHTML = `<span>${descriptionValue}</span><span>$${amountValue}</span><button onclick="removeUnit"> Remove</button>`
+    const removeButton = li.querySelector("button")
+    removeButton.addEventListener("click", function(){
+        removeExpense(li)
+    })
     expenseList.appendChild(li)
+}
+
+function removeExpense(li){
+    expenseList.removeChild(li)
+    console.log("Button Clicked")
 }
 
 // input field must be cleared
